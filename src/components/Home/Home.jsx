7b10e4c5fd8854595cc28e6cf8145ee8f863b394
abhishek2001kene <abhi2001kene@gmail.com>
2024-11-15 23:13:32 +0530

    first commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import TextChange from "./TextChanger";
-import me from "../../assets/me.png";
 import { NavLink } from "react-router-dom";
 function Home() {
   return (
